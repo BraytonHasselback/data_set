6a02b3291a73fd3b767c71ddbc9ba4868972ef6f
BTHass01 <brayton.hasselback@stu.jefferson.kyschools.us>
2018-12-18 08:30:09 -0500

    first

---
+++ main.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import csv
+x = []
+y = []
+filename = 'NBA_season1718_salary.csv'
+with open(filename) as f:
+   reader = csv.reader(f)
+   header_row = next(reader)
+   for index, column_header in enumerate(header_row):
+       print(index, column_header)
+   for row in reader:
+       x.append(row[0])
+       y.append(row[1])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
