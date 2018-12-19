0278807b31a8b1bf38999b6e775b03d6eaa952fe
BTHass01 <brayton.hasselback@stu.jefferson.kyschools.us>
2018-12-19 08:30:42 -0500

    final commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+## Bar Graphs the top 5 Salary's in the current NBA
+
 import matplotlib.pyplot as plt
 import csv
 x = []
@@ -9,9 +11,10 @@ with open(filename) as f:
    for index, column_header in enumerate(header_row):
        print(index, column_header)
    for row in reader:
-       x.append(row[0])
-       y.append(row[1])
+       x.append(row[1])
+       y.append(row[3])
 plt.xlabel('x')
 plt.ylabel('y')
+plt.bar(x,y)
 plt.show()
 
